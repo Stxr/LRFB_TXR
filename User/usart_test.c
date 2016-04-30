115b387043ef63df82b92fca8b7a127faf7cf1e2
Stxr <star_tang_happy@126.com>
2016-05-01 00:07:33 +0800

    把printf重定义从usart3改成了usart1

--- User/usart_test.c
+++ User/usart_test.c
@@ -40,54 +40,54 @@ void USART1_Config(void)
 	NVIC_InitStructure.NVIC_IRQChannelCmd=ENABLE;
 	NVIC_Init(&NVIC_InitStructure);
 }
-void USART3_Config(void)
-{
-	GPIO_InitTypeDef GPIO_InitStructure;
-	USART_InitTypeDef USART_InitStructure;
-	NVIC_InitTypeDef NVIC_InitStructure;
-
-	/* config USART3 clock */
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO| RCC_APB2Periph_GPIOD, ENABLE);
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3,ENABLE);
-	GPIO_PinRemapConfig(GPIO_FullRemap_USART3, ENABLE);
-
-	/* USART3 GPIO config */
-	/* Configure USART3 Tx (PD.08) as alternate function push-pull */
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOD, &GPIO_InitStructure);
-	/* Configure USART3 Rx (PD.9) as input floating */
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
-	GPIO_Init(GPIOD, &GPIO_InitStructure);
-
-	/* USART3 mode config */
-	USART_InitStructure.USART_BaudRate = 9600;                  //波特率
-	USART_InitStructure.USART_WordLength = USART_WordLength_8b;   //传输数据位数
-	USART_InitStructure.USART_StopBits = USART_StopBits_1;        //停止位1
-	USART_InitStructure.USART_Parity = USART_Parity_No ;          //不用校验位
-	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;  //不用流量控制
-	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;  //使用接收和发送功能
-	USART_Init(USART3, &USART_InitStructure);
-	USART_Cmd(USART3, ENABLE);
-	USART_ITConfig(USART3, USART_IT_RXNE, ENABLE);     //串口3 接收中断 使能
-
-	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);   //设置中断配置为第2组，4位设置抢占优先级，4位设置响应优先级
-
-	NVIC_InitStructure.NVIC_IRQChannel=USART3_IRQn;  //串口中断3
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=2;  //最低优先级防止掉台
-	NVIC_InitStructure.NVIC_IRQChannelSubPriority=0;
-	NVIC_InitStructure.NVIC_IRQChannelCmd=ENABLE;
-	NVIC_Init(&NVIC_InitStructure);
-	TIM4_Init(100-1,7200-1);		//10ms中断
-	usart3_rx=0;		//清零
-	TIM4_Set(0);			//关闭定时器4
-}
+// void USART3_Config(void)
+// {
+// 	GPIO_InitTypeDef GPIO_InitStructure;
+// 	USART_InitTypeDef USART_InitStructure;
+// 	NVIC_InitTypeDef NVIC_InitStructure;
+//
+// 	/* config USART3 clock */
+// 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO| RCC_APB2Periph_GPIOD, ENABLE);
+// 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3,ENABLE);
+// 	GPIO_PinRemapConfig(GPIO_FullRemap_USART3, ENABLE);
+//
+// 	/* USART3 GPIO config */
+// 	/* Configure USART3 Tx (PD.08) as alternate function push-pull */
+// 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
+// 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+// 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+// 	GPIO_Init(GPIOD, &GPIO_InitStructure);
+// 	/* Configure USART3 Rx (PD.9) as input floating */
+// 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+// 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+// 	GPIO_Init(GPIOD, &GPIO_InitStructure);
+//
+// 	/* USART3 mode config */
+// 	USART_InitStructure.USART_BaudRate = 9600;                  //波特率
+// 	USART_InitStructure.USART_WordLength = USART_WordLength_8b;   //传输数据位数
+// 	USART_InitStructure.USART_StopBits = USART_StopBits_1;        //停止位1
+// 	USART_InitStructure.USART_Parity = USART_Parity_No ;          //不用校验位
+// 	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;  //不用流量控制
+// 	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;  //使用接收和发送功能
+// 	USART_Init(USART3, &USART_InitStructure);
+// 	USART_Cmd(USART3, ENABLE);
+// 	USART_ITConfig(USART3, USART_IT_RXNE, ENABLE);     //串口3 接收中断 使能
+//
+// 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);   //设置中断配置为第2组，4位设置抢占优先级，4位设置响应优先级
+//
+// 	NVIC_InitStructure.NVIC_IRQChannel=USART3_IRQn;  //串口中断3
+// 	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=2;  //最低优先级防止掉台
+// 	NVIC_InitStructure.NVIC_IRQChannelSubPriority=0;
+// 	NVIC_InitStructure.NVIC_IRQChannelCmd=ENABLE;
+// 	NVIC_Init(&NVIC_InitStructure);
+// 	TIM4_Init(100-1,7200-1);		//10ms中断
+// 	usart3_rx=0;		//清零
+// 	TIM4_Set(0);			//关闭定时器4
+// }
 void USART_Config(void)
 {
 	USART1_Config();
-	USART3_Config();
+//	USART3_Config();
 }
 //设置TIM4的开关
 //sta:0，关闭;1,开启;
@@ -129,10 +129,10 @@ void TIM4_Init(u16 arr,u16 psc)
 int fputc(int ch, FILE *f)
 {
 	/* 发送一个字节数据到USART1 */
-	USART_SendData(USART3, (uint8_t) ch);
+	USART_SendData(USART1, (uint8_t) ch);
 
 	/* 等待发送完毕 */
-	while (USART_GetFlagStatus(USART3, USART_FLAG_TXE) == RESET);
+	while (USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
 
 	return (ch);
 }
@@ -141,8 +141,8 @@ int fputc(int ch, FILE *f)
 int fgetc(FILE *f)
 {
 	/* 等待串口1输入数据 */
-	while (USART_GetFlagStatus(USART3, USART_FLAG_RXNE) == RESET);
+	while (USART_GetFlagStatus(USART1, USART_FLAG_RXNE) == RESET);
 
-	return (int)USART_ReceiveData(USART3);
+	return (int)USART_ReceiveData(USART1);
 }
 /*********************************************END OF FILE**********************/
