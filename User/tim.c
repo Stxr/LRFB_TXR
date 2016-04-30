c283405c51865e5eb8b31a38f6f7d82e1c49eb12
Stxr <star_tang_happy@126.com>
2016-05-01 00:06:36 +0800

    修复了tim2的一个bug

--- User/tim.c
+++ User/tim.c
@@ -4,7 +4,7 @@ void TIM2_Init()
     NVIC_InitTypeDef NVIC_InitStructure;
     TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
 
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3,ENABLE);
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2,ENABLE);
     TIM_TimeBaseStructure.TIM_Period=2000-1; //计数到2000停止
     TIM_TimeBaseStructure.TIM_Prescaler=36000-1;// 72MHz/3600=2KHz
     TIM_TimeBaseStructure.TIM_ClockDivision=TIM_CKD_DIV1;;
@@ -12,7 +12,7 @@ void TIM2_Init()
     TIM_TimeBaseInit(TIM2,&TIM_TimeBaseStructure);
 
     TIM2_Set(0);
-    NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
     NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=1 ;//抢占优先级4
     NVIC_InitStructure.NVIC_IRQChannelSubPriority = 2;		//子优先级3
     NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;			//IRQ通道使能
