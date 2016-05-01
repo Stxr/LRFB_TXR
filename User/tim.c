56c93d1e813f2bb0bd0e8caa51420e05f5bfb47c
Stxr <star_tang_happy@126.com>
2016-05-01 18:24:18 +0800

    小改动

--- User/tim.c
+++ User/tim.c
@@ -5,16 +5,18 @@ void TIM2_Init()
     TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
 
     RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2,ENABLE);
-    TIM_TimeBaseStructure.TIM_Period=2000-1; //计数到2000停止
+    TIM_TimeBaseStructure.TIM_Period=2000-1; //计数到200停止
     TIM_TimeBaseStructure.TIM_Prescaler=36000-1;// 72MHz/3600=2KHz
     TIM_TimeBaseStructure.TIM_ClockDivision=TIM_CKD_DIV1;;
     TIM_TimeBaseStructure.TIM_CounterMode=TIM_CounterMode_Up;
     TIM_TimeBaseInit(TIM2,&TIM_TimeBaseStructure);
 
     TIM2_Set(0);
+//  	TIM_ITConfig(TIM2,TIM_IT_Update,ENABLE ); //使能指定的TIM4中断,允许更新中断
+
     NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
-    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=1 ;//抢占优先级4
-    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 2;		//子优先级3
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=1 ;//抢占优先级1
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 2;		//子优先级2
     NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;			//IRQ通道使能
     NVIC_Init(&NVIC_InitStructure);	//根据指定的参数初始化VIC寄存器
 }
@@ -22,9 +24,15 @@ void TIM2_Set(u8 sta)
 {
     if(sta)
         {
+//            printf("tim2 on\n");
+	          TIM_ITConfig(TIM2,TIM_IT_Update,ENABLE ); //使能指定的TIM4中断,允许更新中断
             TIM_SetCounter(TIM2,0);//计数器清空
             TIM_Cmd(TIM2, ENABLE); //使能TIMx
         }
     else
-        TIM_Cmd(TIM2, DISABLE);//关闭定时器2
+        {
+//            printf("tim2 off\n");
+            TIM_Cmd(TIM2, DISABLE);//关闭定时器2
+        }
+
 }
--- User/tim.h
+++ User/tim.h
@@ -1,4 +1,5 @@
 #include "stm32f10x.h"
 #include "delay.h"
+//#include "usart_test.h"
 void TIM2_Init(void);
 void TIM2_Set(u8 sta);
